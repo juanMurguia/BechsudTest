a1453f0253ca6ae6a31e9e64ac1e5b9d457a6af2
Juan <juancruzmur@gmail.com>
2024-04-11 00:02:38 -0300

    React Icons instalado

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Box,Heading, flexbox } from '@chakra-ui/react'
+import { Box,Heading,Button,UnorderedList, Link} from '@chakra-ui/react'
+import { FaPhoneAlt } from "react-icons/fa";
+
 
 const Header: React.FC = () => {
   return (
-    <Box bg='#F6FAFB' w='100%' p={4} color='gray' display='flex'>
-      <Heading>Quilmes</Heading>
-      <nav>
-        <a href="#">Inicio</a>
-        <a href="#">Acerca de</a>
-        <a href="#">Contacto</a>
-      </nav>
+    <Box bg='#F6FAFB' w='100%' p={6} color='gray' display='flex' position='fixed' justifyContent='space-between'>
+      <Link href='#' textDecoration='none'><Heading color='gray.800' textDecoration='none'>Quilmes</Heading></Link>
+      <UnorderedList display='flex' flexDirection='row' gap={6} alignItems='center'>
+        <Link href="#" color='gray.800'>Estado</Link>
+        <Link href="#" color='gray.800'>Tareas</Link>
+        <Button rightIcon={<FaPhoneAlt />} colorScheme='blue' variant='outline'>
+            Solicitar asistencia
+        </Button>
+      </UnorderedList>
     </Box>
   );
 };
